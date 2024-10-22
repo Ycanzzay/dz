d6ada1acee6a385c0ce35c285aa3db7d95e3517e
Ycanzzay <144926799+Ycanzzay@users.noreply.github.com>
2024-10-22 18:49:37 +0300

    Add files via upload
    zadanie 4

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp14
+{
+    using System;
+    using System.Collections.Generic;
+
+    namespace Kata
+    {
+        public class FindFirstNonRepeatingLetter
+        {
+            public static char FindFirstNonRepeatingLetterInString(string inputString)
+            {
+                Dictionary<char, int> letterCounts = new Dictionary<char, int>();
+
+                foreach (char letter in inputString.ToLower())
+                {
+                    if (letterCounts.ContainsKey(letter))
+                    {
+                        letterCounts[letter]++;
+                    }
+                    else
+                    {
+                        letterCounts.Add(letter, 1);
+                    }
+                }
+
+                foreach (char letter in inputString.ToLower())
+                {
+                    if (letterCounts[letter] == 1)
+                    {
+                        return letter;
+                    }
+                }
+
+                return ' ';
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
