ed850b4280352983556e177e8a755ccad0ef63d3
Ycanzzay <144926799+Ycanzzay@users.noreply.github.com>
2024-10-22 18:46:14 +0300

    Add files via upload
    zadanie 32

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку:");
+        string input = Console.ReadLine();
+
+        
+        Dictionary<char, int> letterCount = new Dictionary<char, int>();
+
+       
+        foreach (char c in input)
+        {
+            if (char.IsLetter(c)) 
+            {
+                char upperChar = char.ToUpper(c);
+                if (letterCount.ContainsKey(upperChar))
+                {
+                    letterCount[upperChar]++;
+                }
+                else
+                {
+                    letterCount[upperChar] = 1;
+                }
+            }
+        }
+
+        // Выводим результат
+        foreach (var kvp in letterCount)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+    }
+}
