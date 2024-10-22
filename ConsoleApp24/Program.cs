d97ed4b0feb15d4b4db5d473b95a45e0425307ac
Ycanzzay <144926799+Ycanzzay@users.noreply.github.com>
2024-10-22 18:47:47 +0300

    Add files via upload
    zadanie 3

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp15
+{
+    using System;
+
+    namespace Kata
+    {
+        public class ToGamelCase
+        {
+            public static string ToGamelCaseString(string inputString)
+            {
+                string[] words = inputString.Split(' ');
+                string gamelCaseString = "";
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        gamelCaseString += words[i].Substring(0, 1).ToUpper() + words[i].Substring(1).ToLower();
+                    }
+                    else
+                    {
+                        gamelCaseString += words[i].Substring(0, 1).ToUpper() + words[i].Substring(1).ToLower();
+                    }
+                }
+
+                return gamelCaseString;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
