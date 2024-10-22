7b9dda9062b5b545d654484e09f1463686641b4c
Ycanzzay <144926799+Ycanzzay@users.noreply.github.com>
2024-10-22 18:40:55 +0300

    zadanie 1

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the numbers without a space");
+        string input = Console.ReadLine();
+        int[] numbers = input.Split(' ')
+                             .Select(int.Parse).ToArray();
+
+        int[] result = MoveZerosToEnd(numbers);
+        Console.WriteLine("The array after moving the zeros " + string.Join(", ", result));
+    }
+    static int[] MoveZerosToEnd(int[] arr)
+    {
+        int[] nonZero = arr.Where(num => num != 0).ToArray();
+        int zeroCount = arr.Length - nonZero.Length;
+        int[] result = new int[arr.Length];
+        Array.Copy(nonZero, result, nonZero.Length); return result;
+    }
+}
